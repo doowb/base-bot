4325d19b03076e4cafee86d282c0dfb1c8eb530c
Brian Woodward <brian.woodward@gmail.com>
2015-12-21 14:47:33 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * base-bot <https://github.com/doowb/base-bot>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
